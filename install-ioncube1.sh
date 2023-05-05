7b437b063a85f502a82a811ad8e5bbbb12d9ce50
ASAS DEV <115852554+asas-dev-web@users.noreply.github.com>
2023-05-05 00:42:38 -0300

    Update install-ioncube1.sh

--- install-ioncube1.sh
+++ install-ioncube1.sh
@@ -17,4 +17,5 @@ sudo service apache2 restart
 rm -f $HOME/install-ioncube1.sh* > /dev/null 2>&1
 history -c
 clear
-echo "IonCube Instalado com Sucesso!"
+echo -e "IonCube Instalado com Sucesso!" | figlet | boxes -d stone -p a0v0 | lolcat
+#echo "IonCube Instalado com Sucesso!"
