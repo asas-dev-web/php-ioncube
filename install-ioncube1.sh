d59938c95182190bcf9d86f8151bd8d912fd9eb1
ASAS DEV <115852554+asas-dev-web@users.noreply.github.com>
2023-05-05 00:14:43 -0300

    Update install-ioncube1.sh

--- install-ioncube1.sh
+++ install-ioncube1.sh
@@ -6,12 +6,12 @@ cp /tmp/ioncube/ioncube_loader_lin_7.2.so /usr/lib/php/20170718
 cd /etc/php/7.2/apache2
 chmod 777 php.ini
 rm php.ini
-wget https://asasdev.tk/s/renovar/php/php.ini
+wget https://raw.githubusercontent.com/asas-dev-web/php-ioncube/main/php/php.ini
 chmod 777 php.ini
 cd /etc/php/7.2/cli
 chmod 777 php.ini
 rm php.ini
-wget wget https://asasdev.tk/s/renovar/painel/php.ini
+wget https://raw.githubusercontent.com/asas-dev-web/php-ioncube/main/painel/php.ini
 chmod 777 php.ini
 sudo service apache2 restart
 rm -f $HOME/install-ioncube1.sh* > /dev/null 2>&1
